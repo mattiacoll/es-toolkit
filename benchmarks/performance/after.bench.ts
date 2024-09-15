9435065abdbc9f254b2a7f46cef656796047b2b6
mattiacoll <64754556+mattiacoll@users.noreply.github.com>
2024-09-16 11:02:34 +1200

    Update after.bench.ts

--- benchmarks/performance/after.bench.ts
+++ benchmarks/performance/after.bench.ts
@@ -2,7 +2,7 @@ import { bench, describe } from 'vitest';
 import { after as afterToolkit } from 'es-toolkit';
 import { after as afterLodash } from 'lodash';
 
-describe('aftere', () => {
+describe('after', () => {
   bench('es-toolkit/after', () => {
     const add = (a: number, b: number) => a + b;
     const n = 10;
